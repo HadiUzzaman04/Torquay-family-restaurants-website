6859ec15da5ad5b5fa1f468c78c317a184daf3fe
HadiUzzaman04 <hadi24x7@gmail.com>
2022-09-14 18:54:11 +0600

    fixed design

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,19 +1,14 @@
 <footer>
     <div class="container">
-        <div class="row">
-            <div class="col-md-3 footer-item">
+        <div class="footer_main">
+            <div class="footer-item">
                 <h4>Torquay Family Restaurant</h4>
                 <p>Best place to have delicious food and to spend quality time with your family & friends</p>
-                <ul class="social-icons">
-                    <li><a rel="nofollow" href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                </ul>
             </div>
-            <div class="col-md-4 footer-item">
+            <div class="footer-item">
                 <div class="footer-info">
                     <div class="section-title">
-                        <h2>Contact Info</h2>
+                        <h4>Contact Info</h4>
                     </div>
                     <address>
                         <p>Address- 122 Devon St,Torquay SK S0C2L0</p>
@@ -22,10 +17,10 @@
                     </address>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6">
+            <div class="footer-item">
                 <div class="footer-info newsletter-form">
-                    <h2>Quick Links</h2>
-                    <div class="row">
+                    <h4>Quick Links</h4>
+                    <div class="row links">
                         <ul>
                             <li><a href="{{route('home')}}"></i>Home</a></li>
                             <li><a href="{{route('booking')}}"></i>Book A Table</a></li>
@@ -36,6 +31,42 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+        <div class="footer_main_small">
+            <div class="footer-item">
+                <h4>Torquay Family Restaurant</h4>
+                <p>Best place to have delicious food and to spend quality time with your family & friends</p>
+            </div>
+            <div class="footer_connect">
+            <div class="footer-item">
+                <div class="footer-info">
+                    <div class="section-title">
+                        <h4>Contact Info</h4>
+                    </div>
+                    <address>
+                        <p>Address- 122 Devon St,Torquay SK S0C2L0</p>
+                        <p>Phone- 306-400-9220</p>
+                        <p><a href="mailto:contact@company.com">info@torquayfamilyrestaurant.com</a></p>
+                    </address>
+                </div>
+            </div>
+            <div class="footer-item">
+                <div class="footer-info newsletter-form">
+                    <h4>Quick Links</h4>
+                    <div class="row links">
+                        <ul>
+                            <li><a href="{{route('home')}}"></i>Home</a></li>
+                            <li><a href="{{route('booking')}}"></i>Book A Table</a></li>
+                            <li><a href="{{route('menu')}}"></i>Menu</a></li>
+                            <li><a href="{{route('aboutus')}}"></i>About Us</a></li>
+                            <li><a href="{{route('contactus')}}"></i>Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+           
         </div>
     </div>
 </footer>
